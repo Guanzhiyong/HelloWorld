9e6b7fa30c18a29c1584d6d920c2ee2fdde041ff
guanzhiyong <472940004@qq.com>
2020-12-23 22:40:05 +0800

    添加for

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,10 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("bianhua");
-
-        int a = 23;
-        int b  = 33;
-
         for (int i = 0; i <1000 ; i++) {
             System.out.println(i);
         }
