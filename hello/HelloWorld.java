48b397de65df18bacc03af32a2ac98eca047b91a
guanzhiyong <472940004@qq.com>
2020-12-23 22:00:05 +0800

    删除a

--- hello/HelloWorld.java
+++ hello/HelloWorld.java
@@ -1,14 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello");
-        for (int i = 0; i <100 ; i++) {
-            System.out.println("aini");
 
 
-
-            int  a = 100;
-
-
-        }
     }
 }
